7032288ce16dc7bf5300c18809ffa643f5a06a52
Asif <asif.iqbca@gmail.com>
2026-07-16 00:54:28 +0530

    Project foundation complete

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[#09090B] text-white flex items-center justify-center">
+      <h1 className="text-6xl font-bold">
+        Asif Iqbal
+      </h1>
+    </div>
+  );
+}
+
+export default App;
